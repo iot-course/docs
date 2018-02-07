4a80dc3954aa6c83197cff5f488a3521617e5e14
wordyallen <joe.narvaez@gmail.com>
2018-02-07 10:18:48 -0600

    closes #1

--- packages/crowdPay/lib/statuses.js
+++ packages/crowdPay/lib/statuses.js
@@ -1,14 +1,21 @@
+const { asyncRequest } = require('./utils')
+
+
+const getPullNumber = async (head, message) => {
+  const { err, data } = await asyncRequest(`/repos/org/iot/course/pulls?state=open?head=${head}`)
+  console.log({err: err.message, data: Boolean(data)})  
+}
 
 exports.handler = (e, _, cb) => {
 
   const {
     state,
     commit:{ commit:{ message } },
-    branches: [{ name }]
+    branches: [{ name:head }]
   } = JSON.parse(e.body)
 
-  console.log({ state, name, message })
-  // const pullNumber = await getPullNumber()
+  const pullNumber = await getPullNumber(head)
+  // console.log({ pullNumber })
   // state === 'success' && prClose(pullNumber)
 
   cb(null, { statusCode: 200 })
