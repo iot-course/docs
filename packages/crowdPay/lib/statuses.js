aef53912a2217ff5fe3ed0c342a00123cf88e102
wordyallen <joe.narvaez@gmail.com>
2018-02-07 12:04:16 -0600

    closes #1

--- packages/crowdPay/lib/statuses.js
+++ packages/crowdPay/lib/statuses.js
@@ -37,7 +37,7 @@ const getPullNumber = async (head, message) => {
 
   return number
     ? number
-    : console.error('could not match pr body to commit msg');
+    : console.log('could not match pr body to commit msg');
 }
 
 exports.handler = async (e, _, cb) => {
@@ -48,7 +48,7 @@ exports.handler = async (e, _, cb) => {
     branches: [{ name:head }]
   } = JSON.parse(e.body)
 
-
+  console.log({ state })
 
   if (state === 'success') {
     const pullNumber = await getPullNumber(head, message)
@@ -63,3 +63,8 @@ exports.handler = async (e, _, cb) => {
   cb(null, { statusCode: 200 })
 
 }
+
+
+/*
+rem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+*/
