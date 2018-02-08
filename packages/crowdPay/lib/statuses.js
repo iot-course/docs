50674d6f449b7f90c8ce0f938466f76898d974b3
wordyallen <joe.narvaez@gmail.com>
2018-02-08 11:55:05 -0600

    setting up

--- packages/crowdPay/lib/statuses.js
+++ packages/crowdPay/lib/statuses.js
@@ -82,3 +82,36 @@ exports.handler = async (e, _, cb) => {
   cb(null, { statusCode: 200 })
 
 }
+
+/*
+..............
+Serverless: Stack update finished...
+Service Information
+service: crowdpay
+stage: dev
+region: us-east-1
+stack: crowdpay-dev
+api keys:
+  None
+endpoints:
+  ANY - https://ff99j1lzsi.execute-api.us-east-1.amazonaws.com/dev/issues
+  ANY - https://ff99j1lzsi.execute-api.us-east-1.amazonaws.com/dev/pull-requests
+  ANY - https://ff99j1lzsi.execute-api.us-east-1.amazonaws.com/dev/statuses
+functions:
+  issues: crowdpay-dev-issues
+  pull-requests: crowdpay-dev-pull-requests
+  statuses: crowdpay-dev-statuses
+Serverless: Removing old service versions...
+
+~/Build/iot-course/org/packages/crowdPay Signup-and-Login 29s
+❯ msg='setting up' yarn push
+yarn run v1.3.2
+warning ../../package.json: No license field
+$ git add -A && git commit -m "$msg" && git push origin $(git rev-parse --abbrev-ref HEAD)
+On branch Signup-and-Login
+nothing to commit, working tree clean
+error Command failed with exit code 1.
+info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command.
+
+~/Build/iot-course/org/packages/crowdPay Signup-and-Login
+*/
