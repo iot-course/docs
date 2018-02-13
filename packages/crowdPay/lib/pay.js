ea3877bd39eeab55c39040d876e9b90655c83c36
wordyallen <joe.narvaez@gmail.com>
2018-02-13 12:42:46 -0600

    setting up

--- packages/crowdPay/lib/pay.js
+++ packages/crowdPay/lib/pay.js
@@ -21,16 +21,58 @@ Explore
  Sign out
 1
 0 0 iot-course/org
- Code  Issues 1  Pull requests 0  Projects 1  Wiki  Insights  Settings
+ Code  Issues 2  Pull requests 1  Projects 1  Wiki  Insights  Settings
+Signup-and-Login #178
+ Closed	wordyallen  wants to merge 2 commits into master from Signup-and-Login
++22 −22
+ Conversation 1   Commits 2   Files changed 1
+Conversation
+ Reviewers
+ @TA-Bot TA-Bot
 
-is:pr is:open
-LabelsMilestones
+ Assignees
+No one—
+ Labels
+None yet
+ Projects
+None yet
+ Milestone
+No milestone
+Notifications
+You’re receiving notifications because you modified the open/close state.
+2 participants
+@wordyallen @TA-Bot
+ Lock conversation
+ @wordyallen
+ Owner
+wordyallen commented 10 seconds ago
+closes #1
+
+wordyallen added some commits 2 minutes ago
+ @wordyallen
+Merge branch 'master' of https://github.com/iot-course/org into Signu…  …
+63ea628
+ @wordyallen
+setting up
+e3e1148
+ @TA-Bot
+TA-Bot approved these changes just now
+Your code is adequate enough given the
+limitations of your species.
+
+ @wordyallen wordyallen closed this just now
+Closed with unmerged commits
+This pull request is closed, but the Signup-and-Login branch has unmerged commits.
+
+@wordyallen
 
 
-There aren’t any open pull requests.
-Use the links above to find what you’re looking for, or try a new search query. The Filters menu is also super helpful for quickly finding issues most relevant to you.
 
- ProTip! Bookmark issues and pull requests to revisit later.
+
+
+Leave a comment
+Attach files by dragging & dropping, , or pasting from the clipboard.  Styling with Markdown is supported
+ ProTip! Add .patch or .diff to the end of URLs for Git’s plaintext views.
 © 2018 GitHub, Inc.
 Terms
 Privacy
@@ -43,6 +85,5 @@ Training
 Shop
 Blog
 About
-
 
 */
